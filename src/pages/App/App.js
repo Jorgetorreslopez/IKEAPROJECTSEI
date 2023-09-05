087ee6ffb2a597dffb35d92270f634aaf93fa740
Jorgetorreslopez <usda4ever@gmail.com>
2023-09-04 20:51:28 -0400

    Started Guest User Functionality

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import styles from './App.module.scss';
 import {
 	Routes,
 	Route,
@@ -12,6 +13,39 @@ import * as ordersAPI from '../../utilities/order-api';
 import HomeScreen from '../HomeScreen/HomeScreen';
 
 export default function App() {
+	const [user, setUser] = useState(getUser());
+	const [showCart, setShowCart] = useState(false);
+	const [showUserPanel, setShowUserPanel] = useState(false);
+	const [cart, setCart] = useState(null);
+	const navigate = useNavigate();
+
+	let location = useLocation();
+
+	useEffect(() => {
+		if (!user) {
+		  createGuestUser()
+		}
+	  }, [])
+
+	  async function createGuestUser() {
+		const guestUserData = {
+			username: 'guestuser',
+			email: Math.floor(Math.random() * 100000) + '@guest.com',
+			password: 'guestpassword'
+		} 
+		localStorage.setItem('guestuser', guestUserData.email);
+		const guestUser = await signUp(guestUserData)
+		setUser(guestUser)
+	  }
+
+	  useEffect(() => {
+		async function getCartItems() {
+			const cart = await ordersAPI.getCart()
+			setCart(cart)
+		}
+		getCartItems()
+	  }, [])
+
 	return (
 		<main>
 			<HomeScreen />
