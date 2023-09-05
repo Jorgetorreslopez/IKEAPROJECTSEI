0496a9738edcd8dc87380d08f01fda0d8f40fa36
Jorgetorreslopez <usda4ever@gmail.com>
2023-09-04 21:10:54 -0400

    created guest users

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,7 +1,7 @@
 import styles from './HomeScreen.module.scss'
 
 export default function HomeScreen(props) {
-    console.log(styles)
+    //console.log(styles)
     return (
         <div className={styles.homescreen}>
             <h1>HI</h1>
